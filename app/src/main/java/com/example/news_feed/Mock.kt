4f924f6383168b91aa4dd3062d32dfeb9e4a9875
Seraphic <danilsocol-2002@mail.ru>
2023-04-23 23:56:55 +0500

    fix

--- app/src/main/java/com/example/news_feed/Mock.kt
+++ app/src/main/java/com/example/news_feed/Mock.kt
@@ -12,8 +12,8 @@ class Mock : INetworkSource {
     private fun getTools():List<NewsModel>{
         return listOf(
             NewsModel.DefaultNewsModel("header","gay"),
-            NewsModel.NewsModelImage("header","gay","https://drive.google.com/file/u/1/d/1oA9C6t28y5i1jtDIXc5vUvmG_ZAqeGKX/view?usp=drive_open"),
-            NewsModel.NewsModelCircleImage("Hello","Hello","https://drive.google.com/file/u/1/d/1oA9C6t28y5i1jtDIXc5vUvmG_ZAqeGKX/view?usp=drive_open",true)
+/*            NewsModel.NewsModelImage("header","gay","https://drive.google.com/file/u/1/d/1oA9C6t28y5i1jtDIXc5vUvmG_ZAqeGKX/view?usp=drive_open"),
+            NewsModel.NewsModelCircleImage("Hello","Hello","https://drive.google.com/file/u/1/d/1oA9C6t28y5i1jtDIXc5vUvmG_ZAqeGKX/view?usp=drive_open",true)*/
         )
     }
 }
